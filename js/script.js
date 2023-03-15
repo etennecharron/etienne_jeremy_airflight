afdf979cd7a22cb0c1e67e60f8cf80c6cd2f8579
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-03-15 17:53:46 -0400

    nav_workplace
    changement de la navbar dans le site workplace + rajout de favicon et d'un titre dans le site

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".btnAimer").addEventListener("click",function(){
+    document.querySelector(".btnAimer").classList.toggle(".aimer")
+    console.log(document.querySelector(".btnAimer").classList)
+    
+})
