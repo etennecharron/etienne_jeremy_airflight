39b1371ec6f08d72997c90901ff06d2b7d9f243e
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-03-15 21:23:13 -0400

    correction_btn_like
    correction du bouton pour qu'il change correctement de couleur

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelector(".btnAimer").addEventListener("click",function(){
-    document.querySelector(".btnAimer").classList.toggle(".aimer")
-    console.log(document.querySelector(".btnAimer").classList)
-    
-})
---
+++ js/script_artwork.js
@@ -0,0 +1,7 @@
+
+//change le bouton like couleur lorsqu'on clique dessus
+document.querySelector(".btnAimer").addEventListener("click",function(){
+    document.querySelector(".btnAimer").classList.toggle("aimer")
+})
+
+
