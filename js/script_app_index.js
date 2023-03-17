7ac740ef09d2c7ec329a2cb4ad17a7c7db699fdb
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-03-17 01:32:23 -0400

    swiper
    rajout des swipers qui fonctionne a moitier yey

---
+++ js/script_app_index.js
@@ -0,0 +1,9 @@
+let mySwiperOne = new Swiper(".swiper-filters",{
+slidesPerView:2,
+  spaceBetween:10,
+});
+
+let mySwiperTwo = new Swiper(".swiper-featured-artworks",{
+slidesPerView:2,
+  spaceBetween:0,
+})
